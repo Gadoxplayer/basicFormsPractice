c4b049bad804640f84261f36e175c513c104567b
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2023-10-30 19:47:31 -0500

    finishing practice of driven angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formData', {static: false}) signupForm: NgForm;
+  defaultSubs = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+  isSubmitted = false;
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.subscription = this.signupForm.value.userData.subscription;
+    this.user.password = this.signupForm.value.userData.password;
+    this.signupForm.reset();
+    this.isSubmitted = true;
+    console.log(this.isSubmitted);
+    
+  }
 
 }
